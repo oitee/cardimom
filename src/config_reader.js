7a6662b16eedfb2cc2c338ef6f632b26a7621abe
oitee <oitee.codes@gmail.com>
2021-09-09 14:41:37 +0530

    Modify config-reader module to filter out blogs with inadequate details.

--- src/config_reader.js
+++ src/config_reader.js
@@ -1,12 +1,52 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
-export function reader(path){
-try {
-  const data = readFileSync(path, 'utf8');
-  return data;
-} catch (err) {
-  console.error(err)
+export function reader(path) {
+  try {
+    const crudeList = readFileSync(path, "utf8");
+    let list = JSON.parse(crudeList);
+    let finalList = [];
+    if (Array.isArray(list)) {
+      for (let blog of list) {
+        let currentBlog = {};
+        if (
+          blog.hasOwnProperty("link") &&
+          blog.hasOwnProperty("filter") &&
+          blog.hasOwnProperty("twitter_username")
+        ) {
+          if (typeof blog.link == "string" && blog.link.length != 0) {
+            currentBlog.link = blog.link;
+          }
+          if (typeof blog.filter == "object") {
+            let keysFilter = Object.keys(blog.filter);
+            if (
+              keysFilter.length == 2 &&
+              blog.filter.hasOwnProperty("includes_any") &&
+              blog.filter.hasOwnProperty("excludes_all") && 
+              Array.isArray(blog.filter.includes_any) &&
+              Array.isArray(blog.filter.excludes_all)
+            ) {
+              currentBlog.filter = blog.filter;
+            }
+          }
+          if (
+            typeof blog.twitter_username == "string" &&
+            blog.twitter_username.length != 0
+          ) {
+            currentBlog.twitter_username = blog.twitter_username;
+          }
+        }
+        let keysCurrentBlog = Object.keys(currentBlog);
+        if (keysCurrentBlog.length == 3) {
+          finalList.push(currentBlog);
+        }
+      }
+      return finalList;
+    }
+    throw "list of blogs is not an array:" + list;
+  } catch (err) {
+    console.error(err);
+  }
 }
-}
-//let path = 'blog_spec.json';
-//console.log(reader(path));
+
+
+
