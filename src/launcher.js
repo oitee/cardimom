6d2a777fd69d7550b49f614db81a286f07d7333c
oitee <oitee.codes@gmail.com>
2021-09-09 14:46:56 +0530

    Minor refactoring changes

--- src/launcher.js
+++ src/launcher.js
@@ -2,61 +2,41 @@ import * as feedParser from "./parser.js";
 import * as fetcher from "./fetcher.js";
 import * as config_reader from "./config_reader.js";
 import * as filter from "./filter.js";
+import * as selector from "./select_new_posts.js";
+import * as addNewPosts from "./add_new_posts.js";
 
 
 
-let crudeListOfBlogs = config_reader.reader('blog_spec.json');
-let listOfBlogs = JSON.parse(crudeListOfBlogs);
+let listOfBlogs = config_reader.reader('blog_spec.json');
 
-for (let blog of listOfBlogs){
+async function insertPosts(blog){
   let url = blog.link;
   let feedText = await fetcher.fetcher(url);
-  if (feedText != undefined) {
+  if (feedText !== undefined) {
     try {
        let parsedBlogs = feedParser.parse(feedText);
-       blog.posts = filter.select(parsedBlogs, blog.filter.includes_any, blog.filter.excludes_all);  
+       blog.posts = filter.select(parsedBlogs, blog.filter.includes_any, blog.filter.excludes_all);
+       return blog;  
       }
       catch (e) {
        console.error(url);
        console.error(e);
      }
     }
+    return blog;
 }
 
 
+listOfBlogs = await Promise.all(listOfBlogs.map(insertPosts));
 
-for(let blog of listOfBlogs){
-  for(let filtered of blog.posts){
-    console.log(filtered.title);
-  }
-}
-//console.log(listOfBlogs[0].parsed[0].content);
+listOfBlogs = listOfBlogs.filter(blog => blog.hasOwnProperty("posts"));
 
-// for (let i = 0; i < urlArr.length; i++) {
-//   let currentURL = urlArr[i];
-//   let feedText = await fetcher.fetcher(currentURL);
-//   if (feedText != undefined) {
-//     try {
-//       let feedBlogs = feedParser.parse(feedText);
-//       // if (feedBlogs.content != undefined) {
-//       //   feedBlogs.content.substring(0, 10);
-//       // }
-//       feedBlogs = feedBlogs.map((val) => {
-//         if (val.content != undefined) {
-//           val.content = val.content.substring(0, 10);
-//         }
-//         return val;
-//       });
-//       console.log(currentURL);
-//       console.table(feedBlogs);
-//       console.log("\n");
-//       console.log();
-//       console.log();
-//       console.log();
-//     } catch (e) {
-//       console.error(currentURL);
-//       console.error(e);
-//     }
-//   }
-// }
+let newBlogs = await selector.selectNewPosts(listOfBlogs);
+
+await addNewPosts.addNewPosts(newBlogs);
+
+
+
+
+
 
---
+++ test/config_reader_test.js
@@ -0,0 +1,49 @@
+import * as config_reader from "../src/config_reader.js";
+import * as assert from "assert";
+
+
+describe("test config reader", () => {
+  
+    it("valid blog_specs", () => {
+      assert.equal(config_reader.config_reader("path"), []);
+
+
+      assert.equal(config_reader.config_reader("path2"), []);
+      assert.equal(interpreter.lisp("(* 5 4 3 2 1)"), 120);
+      assert.equal(interpreter.lisp("(* 5 \n 4 \n 3 2 1)"), 120);
+      assert.equal(interpreter.lisp("(/ 2 3)"), 2/3);
+    });
+  
+    it("Nested expressions", () => {
+      assert.equal(interpreter.lisp("(+ (* 8 9) 9)"), 81);
+      assert.equal(interpreter.lisp("(* 10 (+ 11 12) 13)"), 2990);
+      assert.equal(interpreter.lisp("(+ 1 (* 2 3) (* 4 2))"), 15);
+      assert.equal(interpreter.lisp("(+ -1 (* -2 3) (* 4 2))"), 1);
+      assert.equal(interpreter.lisp("(- (+ 1 2 3) (+ 1 2 3))"), 0);
+      assert.equal(interpreter.lisp("(* (+ 1 2 3) (+ 1 2 3))"), 36);
+      assert.equal(interpreter.lisp("(/ (+ 1 2 3) (+ 1 2 3))"), 1);
+    });
+  
+    it("Valid Expressions with variables", () => {
+      assert.equal(interpreter.lisp("(def n 11) (def m 100) (* n m)"), 1100);
+      assert.equal(interpreter.lisp("(def height 10) (def width (+ height 4)) (def height 11) (* height width)"), 154);
+      assert.equal(interpreter.lisp("(+ 1 (def nine 9) (* 4 2) nine)"), 27);
+      assert.equal(interpreter.lisp("(* 1 (def nine 9) (* 4 -2) nine)"), -648);
+      assert.equal(interpreter.lisp("(def nine 9)"), 9);
+      assert.equal(interpreter.lisp("(+ (def nine 9) (def n nine))"), 18);
+      assert.equal(interpreter.lisp("(def nine 9) (def n nine)"), 9); 
+    });
+  
+    it("Invalid expressions", () => {
+      assert.ok(checkInvalidExpression ("(* 9"));
+      assert.ok(checkInvalidExpression ("(* (+ 9"));
+      assert.ok(checkInvalidExpression ("(+ & 6)"));
+      assert.ok(checkInvalidExpression("(! 5 4 3 2 1)"));
+      assert.ok(checkInvalidExpression("(* 5 4 3 2 1))"));
+      assert.ok(checkInvalidExpression("((def nine 9) (def n nine))"));
+      assert.ok(checkInvalidExpression("(* 11 11"));
+      assert.ok(checkInvalidExpression("(((("));
+      assert.ok(checkInvalidExpression("((())"));
+    });
+  });
+   
