474abf9563c774f5df1f6e52d4cef077f5f35640
oitee <oitee.codes@gmail.com>
2021-09-17 18:24:07 +0530

    Build tweetposts

--- src/launcher.js
+++ src/launcher.js
@@ -7,6 +7,13 @@ import * as twitter from "./twitter.js";
 let config = JSON.parse(readFileSync('blog_spec.json', "utf8"));
 let listOfBlogs = config_reader.reader(config);
 
+let twitterCredentials = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY, 
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET, 
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, 
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
+
 db.poolStart(process.env.DATABASE_URL);
 let listOfPosts = await Promise.all(listOfBlogs.map(fetcher.findPosts));
 listOfPosts = listOfPosts.flatMap(post => post);
@@ -14,5 +21,5 @@ listOfPosts = listOfPosts.flatMap(post => post);
 
 db.selectNewPosts(listOfPosts)
   .then(db.addNewPosts)
-  .then(twitter.publishTweets)
+  .then(newPosts => twitter.publishTweets(twitterCredentials, newPosts))
   .then(db.poolEnd);
--- src/twitter.js
+++ src/twitter.js
@@ -1,20 +1,42 @@
 import Twitter from "twitter-lite";
 
-export async function publishTweets(posts) {}
+export async function publishTweets(credentials, posts) {
+  let listOfTweets = keep(posts, post => {
+    let tweet = `New blog post by ${post.twitter_username}: ${post.title} ${post.link}`;
+    if (tweet.length > 280) {
+      tweet = `${post.twitter_username} posted ${post.link}`;
+    }
+    if (tweet.length > 280) {
+      tweet = false;
+    }
+    return tweet;
+  });
+  await Promise.all(listOfTweets.map(tweetPost => tweet(credentials, tweetPost)));
+}
 
-async function tweet() {
+async function tweet(credentials, tweetPost) {
   const client = new Twitter({
     subdomain: "api", // "api" is the default (change for other subdomains)
     version: "1.1", // version "1.1" is the default (change for other subdomains)
-    consumer_key: "", // from Twitter.
-    consumer_secret: "", // from Twitter.
-    access_token_key: "", // from your User (oauth_token)
-    access_token_secret: "", // from your User (oauth_token_secret)
+    consumer_key: credentials.consumerKey, // from Twitter.
+    consumer_secret: credentials.consumerSecret, // from Twitter.
+    access_token_key: credentials.accessTokenKey, // from your User (oauth_token)
+    access_token_secret: credentials.accessTokenSecret, // from your User (oauth_token_secret)
   });
-
-//   await client.post("statuses/update", {
-//     status: "Hello World! Stay tuned, while I get ready to publish interesting blog posts on JavaScript and much more!",
-//     auto_populate_reply_metadata: true,
-//   });
+  console.log(tweetPost);
+  //   await client.post("statuses/update", {
+  //     status: "Hello World! Stay tuned, while I get ready to publish interesting blog posts on JavaScript and much more!",
+  //     auto_populate_reply_metadata: true,
+  //   });
 }
-await tweet();
+
+function keep(list, mapper) {
+  let reducer = (acc, entry) => {
+    let returnValue = mapper(entry);
+    if (returnValue) {
+      acc.push(returnValue);
+    }
+    return acc;
+  };
+  return list.reduce(reducer, []);
+}
